cdad4d6a65356e0711a676f21f92b86ed82e2b45
Zenrac <zenrac@outlook.fr>
2019-04-04 19:27:26 +0200

    Update ddblapi.py

--- ddblapi/ddblapi.py
+++ ddblapi/ddblapi.py
@@ -1,71 +1,39 @@
+import asyncio
 import aiohttp
 import json
 
 
 class DivineAPI:
 
-    def __init__(self, bot_id, api_key):
+    def __init__(self, bot_id: int, api_key: str, loop=None, aiosession=None):
         self.bot_id = int(bot_id)
         self.api_key = api_key
+        self.loop = loop or asyncio.get_event_loop()
+        self.session = aiosession or aiohttp.ClientSession(loop=self.loop)
+        self.url = 'https://divinediscordbots.com/bot/{}/stats'
         self.headers = {
             'authorization': self.api_key,
             'content-type': 'application/json'
         }
-        self.session = aiohttp.ClientSession()
 
-    async def post_stats(self, server_count):
+    async def post_stats(self, server_count: int):
         data = json.dumps({
             'server_count': int(server_count)
         })
-        url = 'https://divinediscordbots.com/bot/{}/stats'.format(self.bot_id)
 
-        async with self.session.post(url, data=data, headers=self.headers) as resp:
-            if resp.status == 200:
-                return {
-                    'response': 'ServerCount Successfully posted !',
-                    'error': (resp.status != 200),
-                    'status': resp.status
-                }
-            else:
-                error_json = await resp.json()
-                return {
-                    'response': error_json['error'],
-                    'error': (resp.status != 200),
-                    'status': resp.status
-                }
+        async with self.session.post(self.url.format(self.bot_id), data=data, headers=self.headers) as resp:
+            return await self.format_answer(resp)
 
-    async def get_stats(self, user_id):
-        url = 'https://divinediscordbots.com/bot/{}/stats'.format(user_id)
+    async def get_stats(self, user_id: int = None):
+        if not user_id:
+            user_id = self.bot_id
 
-        async with self.session.get(url, headers=self.headers) as resp:
-            if resp.status == 200:
-                return {
-                    'response': await resp.json(),
-                    'error': (resp.status != 200),
-                    'status': resp.status
-                }
-            else:
-                error_json = await resp.json()
-                return {
-                    'response': error_json['error'],
-                    'error': (resp.status != 200),
-                    'status': resp.status
-                }
+        async with self.session.get(self.url.format(user_id), headers=self.headers) as resp:
+            return await self.format_answer(resp)
 
-    async def get_votes(self):
-        url = 'https://divinediscordbots.com/bot/{}/votes'.format(self.bot_id)
-
-        async with self.session.get(url, headers=self.headers) as resp:
-            if resp.status == 200:
-                return {
-                    'response': await resp.json(),
-                    'error': (resp.status != 200),
-                    'status': resp.status
-                }
-            else:
-                error_json = await resp.json()
-                return {
-                    'response': error_json['error'],
-                    'error': (resp.status != 200),
-                    'status': resp.status
-                }
+    async def format_answer(self, resp):
+        return {
+            'response': await resp.json(),
+            'error': (resp.status != 200),
+            'status': resp.status
+        }
