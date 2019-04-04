aeb47a223660fda84bcbc92c746205d874a152c7
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-04 18:29:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='pyddblapi',
     packages=['ddblapi'],
-    version='0.0.1',
+    version='0.0.2',
     description='The official Python wrapper of Divine Discord Bot List',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/DivineDiscordBotList/ddblAPI.py',
     author='Sworder71',
     author_email='divinediscordbots@gmail.com',
