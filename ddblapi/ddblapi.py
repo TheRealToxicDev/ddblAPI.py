a837507071910eaa181a698c22bd9e1eaf23b0a0
Sworder <30504258+Sworder71@users.noreply.github.com>
2019-04-04 18:22:50 +0200

    Update ddblapi.py

--- ddblapi/ddblapi.py
+++ ddblapi/ddblapi.py
@@ -11,6 +11,7 @@ class DivineAPI:
             'authorization': self.api_key,
             'content-type': 'application/json'
         }
+        self.session = aiohttp.ClientSession()
 
     async def post_stats(self, server_count):
         data = json.dumps({
@@ -18,37 +19,35 @@ class DivineAPI:
         })
         url = 'https://divinediscordbots.com/bot/{}/stats'.format(self.bot_id)
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, data=data, headers=self.headers) as resp:
-                if resp.status == 200:
-                    return {
-                        'response': 'ServerCount Successfully posted  !',
-                        'error': (resp.status == 200),
-                        'status': resp.status
-                    }
-                else:
-                    error_json = await resp.json()
-                    return {
-                        'response': error_json['error'],
-                        'error': (resp.status == 200),
-                        'status': resp.status
-                    }
+        async with self.session.post(url, data=data, headers=self.headers) as resp:
+            if resp.status == 200:
+                return {
+                    'response': 'ServerCount Successfully posted !',
+                    'error': (resp.status != 200),
+                    'status': resp.status
+                }
+            else:
+                error_json = await resp.json()
+                return {
+                    'response': error_json['error'],
+                    'error': (resp.status != 200),
+                    'status': resp.status
+                }
 
     async def get_stats(self, user_id):
         url = 'https://divinediscordbots.com/bot/{}/stats'.format(user_id)
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=self.headers) as resp:
-                if resp.status == 200:
-                    return {
-                        'response': await resp.json(),
-                        'error': (resp.status == 200),
-                        'status': resp.status
-                    }
-                else:
-                    error_json = await resp.json()
-                    return {
-                        'response': error_json['error'],
-                        'error': (resp.status == 200),
-                        'status': resp.status
-                    }
+        async with self.session.get(url, headers=self.headers) as resp:
+            if resp.status == 200:
+                return {
+                    'response': await resp.json(),
+                    'error': (resp.status != 200),
+                    'status': resp.status
+                }
+            else:
+                error_json = await resp.json()
+                return {
+                    'response': error_json['error'],
+                    'error': (resp.status != 200),
+                    'status': resp.status
+                }
